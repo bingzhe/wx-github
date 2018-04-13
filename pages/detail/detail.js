cbc3195eaed1164644172e60bd498eaef908b2ba
bingzhe <874177815@qq.com>
2018-04-13 15:10:08 +0800

    detail

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -338,6 +338,80 @@ Page({
                     "score": 1
                 }
             ]
-        }
+        },
+        repo: [],
+        prs: [],
+        language: [],
+    },
+    onLoad() {
+        var repo = this.data.repoinfo.filter(item => {
+            return !item.fork
+        })
+        repo.sort((a, b) => {
+            return (this.computePopularity(a) < this.computePopularity(b))
+        })
+
+        var prs = this.collectPr(this.data.prinfo.items)
+        prs.sort(function (p, c) {
+            return p.popularity < c.popularity;
+        })
+
+        this.setData({
+            repo: repo.slice(0, 5),
+            prs: prs.slice(0, 5),
+            language: this.languageHandler(this.data.repoinfo)
+        })
+    },
+    languageHandler(repoinfo) {
+        let language = {},
+            total = 0
+
+        repoinfo.forEach(item => {
+            let lang = item.language
+
+            if (!lang) {
+                return false
+            } else if (!language[lang]) {
+                language[lang] = {
+                    popularity: 1
+                }
+            } else {
+                language[lang] += 1
+            }
+
+            total += 1
+        })
+
+        return Object.keys(language).map(item => {
+            return {
+                name: item,
+                percent: Math.round(language[item].popularity / total * 100),
+                popularity: language[item].popularity
+            }
+        })
+    },
+    collectPr: function (prs) {
+        prs = prs.reduce(function (p, c) {
+            if (!p[c.repository_url]) {
+                p[c.repository_url] = {
+                    popularity: 1
+                };
+            } else {
+                p[c.repository_url].popularity += 1;
+            }
+            return p;
+        }, {});
+
+        return Object.keys(prs).map(function (v) {
+            return {
+                name: v.replace('https://api.github.com/repos/', ''),
+                popularity: prs[v].popularity
+            }
+        });
+    },
+    computePopularity(repo) {
+        return repo.stargazers_count * 2 + parseInt(repo.forks_count)
     }
+
+
 })
